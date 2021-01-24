6bf7edb9282b8d02c30ae72c49169de07c4d63d4
andyko <b08901012@ntu.edu.tw>
2021-01-24 13:40:24 +0800

    final test

---
+++ public/precache-manifest.0021372633108a34f0937d953561bed4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0885d0c92c0fa3956f90ab6282bb355",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ddbc16443b66b7de0d3",
+    "url": "/static/css/main.5f782870.chunk.css"
+  },
+  {
+    "revision": "35a02e1b4ed356b9ea0e",
+    "url": "/static/js/2.48b618df.chunk.js"
+  },
+  {
+    "revision": "06236f42a9781d97809cba117aebbe7a",
+    "url": "/static/js/2.48b618df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ddbc16443b66b7de0d3",
+    "url": "/static/js/main.90ff7449.chunk.js"
+  },
+  {
+    "revision": "09f84651e1e30890f51c",
+    "url": "/static/js/runtime-main.114c921f.js"
+  },
+  {
+    "revision": "add335bb6b7bc7c8e0c90ee601c1e36d",
+    "url": "/static/media/food_upscaled.add335bb.png"
+  },
+  {
+    "revision": "30d479fc5895b65d1c3407ec97aaa781",
+    "url": "/static/media/fork.30d479fc.png"
+  },
+  {
+    "revision": "bc11db055c1ac7c180cb1b3d98e4a3d5",
+    "url": "/static/media/hamburger.bc11db05.jpg"
+  },
+  {
+    "revision": "f379486837b049b05863f05559d1915f",
+    "url": "/static/media/reserve.f3794868.jpg"
+  }
+]);
---
+++ public/precache-manifest.5db278ab7e6abd1381c700759daf8d95.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f210333e1a6439533e453e1e7685f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f66adf77816508e7bf39",
+    "url": "/static/css/main.5f782870.chunk.css"
+  },
+  {
+    "revision": "5c1fba0f46ce15ab486c",
+    "url": "/static/js/2.b5d270a0.chunk.js"
+  },
+  {
+    "revision": "06236f42a9781d97809cba117aebbe7a",
+    "url": "/static/js/2.b5d270a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f66adf77816508e7bf39",
+    "url": "/static/js/main.8210254c.chunk.js"
+  },
+  {
+    "revision": "09f84651e1e30890f51c",
+    "url": "/static/js/runtime-main.114c921f.js"
+  },
+  {
+    "revision": "add335bb6b7bc7c8e0c90ee601c1e36d",
+    "url": "/static/media/food_upscaled.add335bb.png"
+  },
+  {
+    "revision": "30d479fc5895b65d1c3407ec97aaa781",
+    "url": "/static/media/fork.30d479fc.png"
+  },
+  {
+    "revision": "bc11db055c1ac7c180cb1b3d98e4a3d5",
+    "url": "/static/media/hamburger.bc11db05.jpg"
+  },
+  {
+    "revision": "f379486837b049b05863f05559d1915f",
+    "url": "/static/media/reserve.f3794868.jpg"
+  }
+]);
